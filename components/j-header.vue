c24553c7464dd7afe72bbd0b42ef38e2a16e8901
jordan-furr <jordanfurr98@gmail.com>
2024-12-23 18:10:10 +0100

    v xmas

--- components/j-header.vue
+++ components/j-header.vue
@@ -1,10 +1,13 @@
 <template>
-    <div class="header">
-        <p class="mb3">Josephine Furr is an amazing sister! She is currently pursuing a double major in Computer
-            Science and
-            _____ at Saint Louis University Madrid. At SLU, she is president of the Engineering Club and
-            _______. At Saint Louis University Madrid. At SLU, she is president of the Engineering Club and
-            _______.</p>
-        <p>josiefurr@gmail.com</p>
+    <div class="header flex-col web-row">
+        <div class="web-50">
+            <p class="mb3">Josephine Furr is an amazing sister! She is currently pursuing a double major in Engineering
+                and Computer
+                Science with a Minor in Math at Saint Louis University Madrid. At SLU, she is president of the
+                Engineering Club and does a lot of other cool stuff too.</p>
+        </div>
+        <div class="web-50">
+            <p class="web-right">josiefurr@gmail.com</p>
+        </div>
     </div>
 </template>
